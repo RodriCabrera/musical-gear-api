7b8d891befdd8dc6fa4ed13c9ca1d740d5ea9485
Rodrigo <rodrigo1cabrera2@gmail.com>
2024-11-03 17:04:59 +0100

    Remove AppController and AppService: Clean up unused components from the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GuitarsModule } from './guitars/guitars.module';
 import { PrismaService } from './prisma/prisma.service';
 import { BrandsModule } from './brands/brands.module';
@@ -9,7 +7,6 @@ import { PedalsModule } from './pedals/pedals.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), GuitarsModule, BrandsModule, PedalsModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
